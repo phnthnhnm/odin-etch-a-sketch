e41157a0a7cdbf83c512978e035aca9639b42259
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2024-10-31 12:37:09 +0700

    Add 16x16 grid

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('.container')
+
+  for (let i = 0; i < 16 * 16; i++) {
+    const div = document.createElement('div')
+    div.classList.add('grid-item')
+    container.appendChild(div)
+  }
+})
